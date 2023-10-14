238b2ebece210cf7985735134e68c90b4880e227
Sergey Smirnov <sergey.smirnov.92@gmail.com>
2023-10-14 22:19:54 +0300

    initial commit

---
+++ Examples/Example001_HelloConsole/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("1 Hello, World!");
+Console.WriteLine("2 Hello, World!");
+Console.WriteLine("3 Hello, World!");
+Console.WriteLine("4 Hello, World!");
+Console.WriteLine("5 Hello, World!");
+//первый комментарий
