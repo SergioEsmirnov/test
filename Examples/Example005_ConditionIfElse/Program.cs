c473f018dcb6687659cffd2c1da99ebf955fe80d
Sergey Smirnov <sergey.smirnov.92@gmail.com>
2023-10-15 00:02:06 +0300

    Added Example005_IfElse

---
+++ Examples/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "vasya")
+{
+    Console.WriteLine("Super, this is Vasya!");
+}
+else 
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
